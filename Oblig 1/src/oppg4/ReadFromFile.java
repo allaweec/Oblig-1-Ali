1eb2f55317d2caa25bd316b79edf975b50c121d5
Ali B <alibashar95@gmail.com>
2017-01-25 10:49:41 +0100

    Startet med oppg4

--- Oblig 1/src/oppg4/ReadFromFile.java
+++ Oblig 1/src/oppg4/ReadFromFile.java
@@ -1,7 +1,31 @@
 package oppg4;
 
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.InputStream;
+import java.net.URL;
+import java.util.ArrayList;
+import java.util.Scanner;
+
 public class ReadFromFile {
 
+    public static void main(String[] args) {
 
+        Scanner fileReader = null;
+        try {
+            fileReader = new Scanner(new File("src\\oppg4\\SomeFile.txt"));
+        } catch (FileNotFoundException e) {
+            System.out.println("File not found");
+        }
+
+        ArrayList<String> fileToString = new ArrayList<>();
+        while (fileReader.hasNext()) {
+            fileToString.add(fileReader.next());
+        }
+    }
+
+    private static void getNumberOfWordsInString (String s) {
+
+    }
 
 }
