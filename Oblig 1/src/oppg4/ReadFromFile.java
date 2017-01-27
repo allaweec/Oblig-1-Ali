76445486e5b7595b6621e973bba3f7f07c6f7208
Ali B <alibashar95@gmail.com>
2017-01-27 14:50:05 +0100

    Jobbet litt med oppgave 4

--- Oblig 1/src/oppg4/ReadFromFile.java
+++ Oblig 1/src/oppg4/ReadFromFile.java
@@ -20,10 +20,30 @@ public class ReadFromFile {
         while (fileReader.hasNext()) {
             fileToString.add(fileReader.next());
         }
+
+        System.out.println("Characters: " + countCharactersInArrayList(fileToString));
+        System.out.println("Words: " + fileToString.size());
+
     }
 
-    private static void getNumberOfWordsInString (String s) {
+    private static int getNumberOfWordsInString (String s) {
+        String alphabet = "abcdefghijklmnopqrstuvwxyzæøå";
 
+        int numberOfWords = 0;
+        for (int i = 0; i < s.length(); i++) {
+            if (alphabet.contains(Character.toString(s.toLowerCase().charAt(i)))) {
+                numberOfWords++;
+            }
+        }
+        return numberOfWords;
+    }
+
+    private static int countCharactersInArrayList(ArrayList<String> arrayList) {
+        int numberOfWords = 0;
+        for (int i = 0; i < arrayList.size(); i++) {
+            numberOfWords += getNumberOfWordsInString(arrayList.get(i));
+        }
+        return numberOfWords;
     }
 
 }
